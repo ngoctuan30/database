07a2037e1ecbcca78d40bee458a5dff55220e5b9
ngoctuan30 <59278920+ngoctuan30@users.noreply.github.com>
2023-09-02 09:42:29 +0700

    Update Getdatabase.bat

--- Getdatabase.bat
+++ Getdatabase.bat
@@ -14,14 +14,15 @@ wget --user=EAV-0390167873 --password=ua526pkuub -O C:\update\offline_update_eav
 @echo off
 setlocal
 
-set "installDir=C:\Program Files (x86)\7-Zip"
+setlocal
+set "installDir=C:\Program Files\7-Zip"
 if not exist "%installDir%\7z.exe" (
-    wget https://www.7-zip.org/a/7z2301.msi
-    7z2301.msi /q    
+    wget -O C:\Windows\Temp\7z2301.msi https://www.7-zip.org/a/7z2301-x64.msi
+    "C:\Windows\Temp\7z2301-x64.msi" /q    
     echo Cai dat thanh cong.
 ) else (
     echo Da cai roi.
 )
 endlocal
 
-"C:\Program Files (x86)\7-Zip\7z.exe" x -aoa "C:\update\offline_update_eav.zip" -o"C:\update"
+"C:\Program Files\7-Zip\7z.exe" x -aoa "C:\update\offline_update_eav.zip" -o"C:\update"
